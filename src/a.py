c26af045eea46e312abc0d287b2e9afd9619a503
jgaffuri <jgaf@proton.me>
2024-07-15 20:01:40 +0200

    a

--- src/a.py
+++ src/a.py
@@ -1,7 +1,13 @@
 
 
+# check client
+# OL https://openlayers.org/workshop/en/vectortile/
+# leaflet https://www.npmjs.com/package/leaflet-vector-tile-layer
 
-# 
+# Make dummy application - with example
+# Test features
+
+# make gps data vector tiles 
 # Load GPS traces from folder
 
 
