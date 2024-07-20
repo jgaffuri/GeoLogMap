81fa8e1efbab1e730194f780bf37354213fc1898
jgaffuri <jgaf@proton.me>
2024-07-20 21:01:24 +0200

    a

--- src/gps_to_geopackage.py
+++ src/gps_to_geopackage.py
@@ -25,24 +25,25 @@ def parse_gpx(file_path):
                     })
     return traces
 
-
 def parse_tcx(file_path):
     traces = []
-    with open(file_path, 'r') as tcx_file:
-        tree = etree.parse(tcx_file)
-        root = tree.getroot()
+    with open(file_path, 'rb') as tcx_file:
+        content = tcx_file.read()
+        content = content.strip()
+        
+        tree = etree.fromstring(content)
         xmlns = {'ns': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2'}
         
-        for activity in root.findall('ns:Activities/ns:Activity', xmlns):
-            for lap in activity.findall('ns:Lap', xmlns):
+        for activity in tree.findall('.//ns:Activity', namespaces=xmlns):
+            for lap in activity.findall('ns:Lap', namespaces=xmlns):
                 points = []
                 times = []
-                for track in lap.findall('ns:Track/ns:Trackpoint', xmlns):
-                    time = track.find('ns:Time', xmlns).text
-                    pos = track.find('ns:Position', xmlns)
+                for track in lap.findall('ns:Track/ns:Trackpoint', namespaces=xmlns):
+                    time = track.find('ns:Time', namespaces=xmlns).text
+                    pos = track.find('ns:Position', namespaces=xmlns)
                     if pos is not None:
-                        lat = float(pos.find('ns:LatitudeDegrees', xmlns).text)
-                        lon = float(pos.find('ns:LongitudeDegrees', xmlns).text)
+                        lat = float(pos.find('ns:LatitudeDegrees', namespaces=xmlns).text)
+                        lon = float(pos.find('ns:LongitudeDegrees', namespaces=xmlns).text)
                         points.append((lon, lat))
                         times.append(pd.to_datetime(time))
                 if len(points) > 1:
@@ -56,12 +57,14 @@ def parse_tcx(file_path):
                     })
     return traces
 
+
 def create_geopackage_from_gpx_tcx(folder_path, output_file):
     traces = []
     for file in os.listdir(folder_path):
-        if file.endswith(".gpx"):
-            traces.extend(parse_gpx(os.path.join(folder_path, file)))
-        elif file.endswith(".tcx"):
+        #if file.endswith(".gpx"):
+        #    traces.extend(parse_gpx(os.path.join(folder_path, file)))
+        if file.endswith(".tcx"):
+            print(file)
             traces.extend(parse_tcx(os.path.join(folder_path, file)))
 
     # Create a GeoDataFrame
