f677a1c64b3c5c490de43e09d650aa91e7a4d681
jgaffuri <jgaf@proton.me>
2024-07-20 21:27:46 +0200

    a

--- src/gps_to_geopackage.py
+++ src/gps_to_geopackage.py
@@ -4,6 +4,7 @@ from shapely.geometry import LineString
 import gpxpy
 import pandas as pd
 from lxml import etree
+from fitparse import FitFile
 
 
 def parse_gpx(file_path):
@@ -24,6 +25,9 @@ def parse_gpx(file_path):
                         'end_time': end_time
                     })
     return traces
+
+
+
 
 def parse_tcx(file_path):
     traces = []
@@ -58,20 +62,61 @@ def parse_tcx(file_path):
     return traces
 
 
-def create_geopackage_from_gpx_tcx(folder_path, output_file):
+
+
+
+def parse_fit(file_path):
+    traces = []
+    fitfile = FitFile(file_path)
+    points = []
+    times = []
+    for record in fitfile.get_messages('record'):
+        # Get the data from the record
+        lat = None
+        lon = None
+        time = None
+        for record_data in record:
+            if record_data.name == 'position_lat':
+                lat = record_data.value * (180 / 2**31)  # Convert semicircles to degrees
+            elif record_data.name == 'position_long':
+                lon = record_data.value * (180 / 2**31)  # Convert semicircles to degrees
+            elif record_data.name == 'timestamp':
+                time = record_data.value
+        
+        if lat is not None and lon is not None and time is not None:
+            points.append((lon, lat))
+            times.append(pd.to_datetime(time))
+    
+    if len(points) > 1:
+        line = LineString(points)
+        start_time = times[0]
+        end_time = times[-1]
+        traces.append({
+            'geometry': line,
+            'start_time': start_time,
+            'end_time': end_time
+        })
+    
+    return traces
+
+
+
+
+def create_geopackage(folder_path, output_file):
     traces = []
     for file in os.listdir(folder_path):
-        #if file.endswith(".gpx"):
-        #    traces.extend(parse_gpx(os.path.join(folder_path, file)))
-        if file.endswith(".tcx"):
-            print(file)
-            traces.extend(parse_tcx(os.path.join(folder_path, file)))
+        #try:
+            #if file.endswith(".gpx"):
+            #    traces.extend(parse_gpx(os.path.join(folder_path, file)))
+            #if file.endswith(".tcx"):
+            #    traces.extend(parse_tcx(os.path.join(folder_path, file)))
+            if file.endswith(".fit"):
+                traces.extend(parse_fit(os.path.join(folder_path, file)))
+        #except:
+        #    print("Error when parsing file: "+file)
 
-    # Create a GeoDataFrame
     gdf = gpd.GeoDataFrame(traces, crs="EPSG:4326")
-    
-    # Write the GeoDataFrame to a GeoPackage
     gdf.to_file(output_file, layer='gps_traces', driver='GPKG')
 
 
-create_geopackage_from_gpx_tcx("/home/juju/geodata/GPS/traces_export_stava", "/home/juju/geodata/GPS/traces.gpkg")
+create_geopackage("/home/juju/geodata/GPS/traces_export_stava", "/home/juju/geodata/GPS/traces.gpkg")
